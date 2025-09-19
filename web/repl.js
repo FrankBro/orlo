d49a4cd3a7160a99c45c07fcf61a355c99f6b90e
Francois Brodeur <francois.brodeur.87@gmail.com>
2025-09-19 14:21:15 -0400

    try repl

---
+++ web/repl.js
@@ -0,0 +1,12 @@
+import init, { repl } from "../pkg/web.js";
+
+export class Repl {
+  constructor() {
+    this.ready = init(); // load wasm
+  }
+
+  async eval(command) {
+    await this.ready; // wait for wasm to load
+    return repl(command);
+  }
+}
--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,1 +1,43 @@
+use orlo::{env::Env, eval::eval, infer, parser::parse, value::Value};
+use wasm_bindgen::prelude::*;
 
+fn get_type(env: &mut infer::Env, value: &Value) -> Result<String, infer::Error> {
+    let ty = env.infer_value(value)?;
+    env.ty_to_string(&ty)
+}
+
+// pub fn run() {
+//     let mut env = Env::primitive_bindings();
+//     print("orlo>>> ");
+//     let stdin = io::stdin();
+//     let mut type_env = infer::Env::primitive_bindings();
+//     for line in stdin.lock().lines() {
+//         let line = line.unwrap();
+//         let input = line.trim_end();
+//         if input == "quit" {
+//             return;
+//         }
+//         match parse(input) {
+//             Ok(value) => {
+//                 let ty = match get_type(&mut type_env, &value) {
+//                     Ok(ty) => ty,
+//                     Err(e) => {
+//                         println!("Type error: {:?} for {}", e, input);
+//                         String::from("error")
+//                     }
+//                 };
+//                 match eval(&mut env, &value) {
+//                     Ok(value) => println!("(the {} {})", ty, value),
+//                     Err(e) => println!("Eval error: {}", e),
+//                 }
+//             }
+//             Err(e) => println!("Parse error: {:?}", e),
+//         }
+//         print("orlo>>> ");
+//     }
+// }
+
+#[wasm_bindgen]
+pub fn repl(input: &str) -> String {
+    format!("Received {input}")
+}
