83cca79b2d36d7c75b24b4409b7d46b700988b8e
Francois Brodeur <francois.brodeur.87@gmail.com>
2025-10-09 23:30:17 -0400

    Create variant_tests.rs

---
+++ orlo/src/tests/variant_tests.rs
@@ -0,0 +1,16 @@
+use crate::repl::Repl;
+
+#[test]
+fn tests() {
+    let cases: &[(&str, &str)] = &[
+        ("(.a 1)", "(the (.a int . ra) (.a 1))"),
+        // match next
+    ];
+    let mut repl = Repl::default();
+    for (input, expected) in cases {
+        match repl.handle_input(input) {
+            Ok(the) => assert_eq!(&the.to_string(), expected, "for input {}", input),
+            Err(e) => panic!("Error: {:?} for input {}", e, input),
+        }
+    }
+}
